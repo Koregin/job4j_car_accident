0e3d0b21012e5073ad23440f2b22ab8386776634
Evgeny Koregin <mnemonic78@yandex.ru>
2022-06-05 20:55:01 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> names = List.of("Petr Arsentev", "Ivan Petrov", "Tom Cruise", "Bruce Willis", "Linux Torwalds");
+        model.addAttribute("names", names);
         return "index";
     }
 }

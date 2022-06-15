861c318093cf4c1c3f7ea0a5ce0344d865d15154
Evgeny Koregin <mnemonic78@yandex.ru>
2022-06-15 11:32:34 +0300

    1. Spring ORM [#2093 #312237]

--- src/main/java/ru/job4j/accident/model/Accident.java
+++ src/main/java/ru/job4j/accident/model/Accident.java
@@ -26,7 +26,7 @@ public class Accident {
     @JoinColumn(name = "accident_type_id")
     private AccidentType type;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "accident_rule",
             joinColumns = @JoinColumn(name = "accident_id"),
--- src/main/java/ru/job4j/accident/repository/AccidentHibernate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentHibernate.java
@@ -2,12 +2,14 @@ package ru.job4j.accident.repository;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.model.Rule;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class AccidentHibernate {
@@ -18,53 +20,49 @@ public class AccidentHibernate {
     }
 
     public Accident save(Accident accident) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.save(accident);
-            session.getTransaction().commit();
-            return accident;
-        }
+        Integer id = (Integer) this.tx(session -> session.save(accident));
+        accident.setId(id);
+        return accident;
     }
 
     public List<Accident> getAll() {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("from Accident", Accident.class)
-                    .getResultList();
-        }
+        return this.tx(session -> session.createQuery("select distinct a from Accident a join fetch a.rules order by a.id", Accident.class).getResultList());
     }
 
     public Accident replace(Accident accident) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.update(accident);
-            session.getTransaction().commit();
-            return accident;
-        }
+        return (Accident) this.tx(session -> session.merge(accident));
     }
 
     public Accident findAccidentById(int id) {
-        try (Session session = sf.openSession()) {
-            return session.get(Accident.class, id);
-        }
+        return this.tx(session -> session.createQuery("select a from Accident a join fetch a.rules where a.id = :fId", Accident.class)
+                .setParameter("fId", id)
+                .uniqueResult());
     }
 
     public List<AccidentType> findAllTypes() {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("from AccidentType", AccidentType.class)
-                    .getResultList();
-        }
+        return this.tx(session -> session.createQuery("from AccidentType", AccidentType.class).getResultList());
     }
 
     public List<Rule> findAllRules() {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("from Rule", Rule.class)
-                    .getResultList();
-        }
+        return this.tx(session -> session.createQuery("from Rule", Rule.class).getResultList());
     }
 
     public Rule findRuleById(int id) {
-        try (Session session = sf.openSession()) {
-            return session.get(Rule.class, id);
+        return this.tx(session -> session.get(Rule.class, id));
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
         }
     }
 }

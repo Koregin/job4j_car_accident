5db9d0adafb9ea75465da26c79d698c6fc5e7b37
Evgeny Koregin <mnemonic78@yandex.ru>
2022-06-17 10:51:01 +0300

    2. Spring Data [#296073 #313056]

--- src/main/java/ru/job4j/accident/service/AccidentService.java
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -5,6 +5,8 @@ import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.model.Rule;
 import ru.job4j.accident.repository.AccidentRepository;
+import ru.job4j.accident.repository.AccidentTypeRepository;
+import ru.job4j.accident.repository.RuleRepository;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
@@ -14,47 +16,54 @@ import java.util.Set;
 
 @Service
 public class AccidentService {
-    private final AccidentRepository repository;
+    private final AccidentRepository accidentRepo;
+    private final AccidentTypeRepository accidentTypeRepo;
+    private final RuleRepository ruleRepo;
 
-    public AccidentService(AccidentRepository repository) {
-        this.repository = repository;
+    public AccidentService(AccidentRepository accidentRepo,
+                           AccidentTypeRepository accidentTypeRepo,
+                           RuleRepository ruleRepo) {
+        this.accidentRepo = accidentRepo;
+        this.accidentTypeRepo = accidentTypeRepo;
+        this.ruleRepo = ruleRepo;
     }
+
 
     @Transactional
     public List<Accident> findAll() {
-        return repository.findAll();
+        return accidentRepo.findAll();
     }
 
     @Transactional
     public void create(Accident accident) {
-        repository.save(accident);
+        accidentRepo.save(accident);
     }
 
     @Transactional
     public void update(Accident accident) {
-        repository.save(accident);
+        accidentRepo.save(accident);
     }
 
     @Transactional
     public Accident findAccidentById(int id) {
-        return repository.findById(id).orElse(null);
+        return accidentRepo.findById(id).orElse(null);
     }
 
     @Transactional
     public List<AccidentType> findAllTypes() {
-        return repository.findAllTypes();
+        return accidentTypeRepo.findAll();
     }
 
     @Transactional
     public List<Rule> findAllRules() {
-        return repository.findAllRules();
+        return ruleRepo.findAll();
     }
 
     @Transactional
     public Set<Rule> findRulesForAccident(String[] ids) {
         Set<Rule> rules = new HashSet<>();
         if (ids != null) {
-            Arrays.stream(ids).forEach(id -> rules.add(repository.findRuleById(Integer.parseInt(id))));
+            Arrays.stream(ids).forEach(id -> rules.add(ruleRepo.findById(Integer.parseInt(id)).orElse(null)));
         }
         return rules;
     }
